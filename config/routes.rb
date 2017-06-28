5641b6c211e4afde98f25ec98839091b4f1a423e
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-06-28 14:29:01 +0000

    Almost there

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  get "book-search", to: "books#search"
+  get "results", to: "books#results"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |book_items|
+    Book.create!(
+        title: "Shoomp",
+        sales: 20
+        ) 
+end
+
+5.times do |book_items|
+    Book.create!(
+        title: "Shomp",
+        sales: 10
+        ) 
+end
+
+puts "10 books were made"
